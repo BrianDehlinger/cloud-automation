c34aea2b48592dc28be7d6cde88ec24bdca528c6
Brian Dehlinger <briandehlinger@pop-os.localdomain>
2021-01-21 17:26:57 -0600

    Add parameters for routing tables to approve_vpcpeering_request.sh

--- gen3/bin/approve_vpcpeering_request.sh
+++ gen3/bin/approve_vpcpeering_request.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-    echo "USAGE: $0 name_of_child_vpc"
+if [ $# -gt 3 ] || [ $# -lt 1 ] ; then
+    echo "USAGE: $0 name_of_child_vpc [route_table_one] [route_table_two]"
     exit 1
 fi
+ROUTE_TABLE_ONE=${2:-rtb-23b6685f}
+ROUTE_TABLE_TWO=${3:-rtb-rtb-7ee06301}
+
+echo "First Route Table is: $ROUTE_TABLE_ONE"
+echo "Second Route Table is $ROUTE_TABLE_TWO"
 vpcpeering_pending_acceptence=$(aws ec2 describe-vpc-peering-connections --filters Name=status-code,Values=pending-acceptance --output text)
 if [ -n "$vpcpeering_pending_acceptence" ]; then
 child_vpc_name=$1
@@ -14,8 +19,8 @@ aws ec2 create-tags --resources $vpcpeerconnid --tags Key=Name,Value="VPC peerin
 echo "The vpc peering connection request for id $vpcpeerconnid was accepted"
 
 
-aws ec2 create-route --route-table-id rtb-23b6685f --destination-cidr-block $vpccidrblock --vpc-peering-connection-id $vpcpeerconnid
-aws ec2 create-route --route-table-id rtb-7ee06301 --destination-cidr-block $vpccidrblock --vpc-peering-connection-id $vpcpeerconnid
+aws ec2 create-route --route-table-id $ROUTE_TABLE_ONE --destination-cidr-block $vpccidrblock --vpc-peering-connection-id $vpcpeerconnid
+aws ec2 create-route --route-table-id $ROUTE_TABLE_TWO --destination-cidr-block $vpccidrblock --vpc-peering-connection-id $vpcpeerconnid
 
 echo "The route for the child vpc $child_vpc_name cidr $vpccidrblock was added"
 exit 1
